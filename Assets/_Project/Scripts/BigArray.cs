777d37421c56b73db22dddf68f333d13bb734efa
ElasticSea <info@elasticsea.com>
2021-03-27 01:35:04 +0100

    Enumerator is way to slow for anything useful here

--- Assets/_Project/Scripts/BigArray.cs
+++ Assets/_Project/Scripts/BigArray.cs
@@ -1,20 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
-
-public class BigArray<T> : IEnumerable<T>
+public class BigArray<T>
 {
     private const int ChunkSize = 1024 * 1024; // Must be 2^n in order for the fastmodule to work and under 0X7FEFFFFF
     private const int FastModulo = ChunkSize - 1;
 
     public readonly T[][] Data;
-        
+
     public readonly long Length;
 
     public BigArray(long length)
     {
         Length = length;
-        var chunk = (int)(length / ChunkSize);
-        var index = (int)(length % ChunkSize);
+        var chunk = (int) (length / ChunkSize);
+        var index = (int) (length % ChunkSize);
 
         Data = new T[chunk + 1][];
         var i = 0;
@@ -22,6 +19,7 @@ public class BigArray<T> : IEnumerable<T>
         {
             Data[i] = new T[ChunkSize];
         }
+
         Data[i] = new T[index];
     }
 
@@ -29,28 +27,15 @@ public class BigArray<T> : IEnumerable<T>
     {
         get
         {
-            var chunk = (int)(i / ChunkSize);
-            var index = (int)(i & FastModulo);
+            var chunk = (int) (i / ChunkSize);
+            var index = (int) (i & FastModulo);
             return Data[chunk][index];
         }
         set
         {
-            var chunk = (int)(i / ChunkSize);
-            var index = (int)(i & FastModulo);
+            var chunk = (int) (i / ChunkSize);
+            var index = (int) (i & FastModulo);
             Data[chunk][index] = value;
         }
-    }
-
-    public IEnumerator<T> GetEnumerator()
-    {
-        for (long i = 0; i < Length; i++)
-        {
-            yield return this[i];
-        }
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
     }
 }
