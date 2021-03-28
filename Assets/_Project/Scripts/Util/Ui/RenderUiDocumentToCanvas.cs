5b63d275643305141055ec36b998c5c4a6f1dccc
ElasticSea <info@elasticsea.com>
2021-03-28 02:44:51 +0100

    Render ui document to render texture because of stencil bug on vr

---
+++ Assets/_Project/Scripts/Util/Ui/RenderUiDocumentToCanvas.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+namespace Util.Ui
+{
+    public class RenderUiDocumentToCanvas : MonoBehaviour
+    {
+        [SerializeField] private UIDocument uiDocument;
+    
+        private void Awake()
+        {
+            var rt = new RenderTexture(Screen.width, Screen.height, 32);
+            var canvas = new GameObject().AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            var rawImage = new GameObject().AddComponent<RawImage>();
+            var rawImageRt = rawImage.GetComponent<RectTransform>();
+            rawImageRt.anchorMin = Vector2.zero;
+            rawImageRt.anchorMax = Vector2.one;
+            rawImageRt.offsetMin = Vector2.zero;
+            rawImageRt.offsetMax = Vector2.zero;
+            rawImage.transform.SetParent(canvas.transform,false);
+            rawImage.texture = rt;
+
+            var uiDocumentPanelSettings = Instantiate(uiDocument.panelSettings);
+            uiDocumentPanelSettings.targetTexture = rt;
+            uiDocument.panelSettings = uiDocumentPanelSettings;
+        }
+    }
+}
