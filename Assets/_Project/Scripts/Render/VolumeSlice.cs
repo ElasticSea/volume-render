cbf3cc98a2f7e5becc8fe4ecb848a6de53132275
ElasticSea <info@elasticsea.com>
2021-06-11 02:29:40 +0200

    19 - Only cut when held in VR

--- Assets/_Project/Scripts/Render/VolumeSlice.cs
+++ Assets/_Project/Scripts/Render/VolumeSlice.cs
@@ -1,10 +1,13 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR;
+using Util;
 
 namespace Render
 {
     public class VolumeSlice : MonoBehaviour
     {
+        [SerializeField] private bool isRightController;
         [SerializeField] private VolumeRenderManager vrm;
 
         private void Awake()
@@ -18,7 +21,14 @@ namespace Render
 
         private void Update()
         {
-            vrm.Cut(transform.position, transform.up);
+            var orientationCharacteristics = isRightController ? InputDeviceCharacteristics.Right : InputDeviceCharacteristics.Left;
+            var controllerCharacteristics = orientationCharacteristics | InputDeviceCharacteristics.Controller;
+            var isHeld = controllerCharacteristics.GetDevice().TryGetFeatureValue(CommonUsages.trigger, out var heldValue) && heldValue > 0.7f;
+
+            if (isHeld)
+            {
+                vrm.Cut(transform.position, transform.up);
+            }
         }
     }
 }
