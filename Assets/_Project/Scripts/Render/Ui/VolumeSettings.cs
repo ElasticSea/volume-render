e133820f07fe5e4cc019cfc8fc29edde471ffa6f
ElasticSea <info@elasticsea.com>
2021-06-22 11:54:22 +0200

    Render volume can be null

--- Assets/_Project/Scripts/Render/Ui/VolumeSettings.cs
+++ Assets/_Project/Scripts/Render/Ui/VolumeSettings.cs
@@ -8,50 +8,89 @@ namespace Render.Ui
         [SerializeField] private VolumeRenderManager volumeRenderManager;
 
         public bool IsActive => true;
-        
+
         public float Size
         {
-            get => volumeRenderManager.Size;
-            set => volumeRenderManager.Size = value;
+            get => volumeRenderManager?.Size ?? 0f;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.Size = value;
+                }
+            }
         }
 
         public float Alpha
         {
-            get => volumeRenderManager.VolumeRender.Alpha;
-            set => volumeRenderManager.VolumeRender.Alpha = value;
+            get => volumeRenderManager.VolumeRender?.Alpha ?? 0f;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.VolumeRender.Alpha = value;
+                }
+            }
         }
 
         public float AlphaThreshold
         {
-            get => volumeRenderManager.VolumeRender.AlphaThreshold;
-            set => volumeRenderManager.VolumeRender.AlphaThreshold = value;
+            get => volumeRenderManager.VolumeRender?.AlphaThreshold ?? 0f;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.VolumeRender.AlphaThreshold = value;
+                }
+            }
         }
 
         public float StepDistance
         {
-            get => volumeRenderManager.VolumeRender.StepDistance;
-            set => volumeRenderManager.VolumeRender.StepDistance = value;
+            get => volumeRenderManager.VolumeRender?.StepDistance ?? 0f;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.VolumeRender.StepDistance = value;
+                }
+            }
         }
-
 
         public float ClipMinimumThreashold
         {
-            get => volumeRenderManager.VolumeRender.ClipMinimumThreashold;
-            set => volumeRenderManager.VolumeRender.ClipMinimumThreashold = value;
+            get => volumeRenderManager.VolumeRender?.ClipMinimumThreashold ?? 0f;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.VolumeRender.ClipMinimumThreashold = value;
+                }
+            }
         }
-
 
         public float ClipMaximumThreashold
         {
-            get => volumeRenderManager.VolumeRender.ClipMaximumThreashold;
-            set => volumeRenderManager.VolumeRender.ClipMaximumThreashold = value;
+            get => volumeRenderManager.VolumeRender?.ClipMaximumThreashold ?? 0f;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.VolumeRender.ClipMaximumThreashold = value;
+                }
+            }
         }
-
 
         public int MaxStepThreshold
         {
-            get => volumeRenderManager.VolumeRender.MaxStepThreshold;
-            set => volumeRenderManager.VolumeRender.MaxStepThreshold = value;
+            get => volumeRenderManager.VolumeRender?.MaxStepThreshold  ?? 0;
+            set
+            {
+                if (volumeRenderManager.VolumeRender)
+                {
+                    volumeRenderManager.VolumeRender.MaxStepThreshold = value;
+                }
+            }
         }
 
         public IEnumerable<RenderPreset> RenderPresets => volumeRenderManager.RenderPresets;
